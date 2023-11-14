c289ced39d59d99d6e558e8f56ee3e2fb8d63b7c
RomanMysyura <rmysyura@cendrassos.net>
2023-11-14 18:25:34 +0100

    first version

--- navbar.php
+++ navbar.php
@@ -3,7 +3,7 @@
         <div class="container mx-auto flex justify-between items-center">
             <a href="#" class="text-white font-bold text-xl">Logo</a>
             <div class="space-x-4">
-                <a href="#" class="text-white">Inicio</a>
+                <a href="#" class="text-white">Inici</a>
                 <a href="#" class="text-white">Acerca de</a>
                 <a href="#" class="text-white">Servicios</a>
                 <a href="#" class="text-white">Contacto</a>
