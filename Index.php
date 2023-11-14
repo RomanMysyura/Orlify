747833d4261247be3e1f2bf910c1f553a7d8f8b9
RomanMysyura <rmysyura@cendrassos.net>
2023-11-14 18:24:42 +0100

    first version

--- Index.php
+++ Index.php
@@ -1,1 +1,17 @@
-<!-- Orlifyyy -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
+    <title>Document</title>
+</head>
+<body class="bg-gray-100">
+    <?php include 'navbar.php';?>
+
+    <div class="container mx-auto mt-8">
+        
+    </div>
+
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,12 @@
+
+    <nav class="bg-blue-500 p-4">
+        <div class="container mx-auto flex justify-between items-center">
+            <a href="#" class="text-white font-bold text-xl">Logo</a>
+            <div class="space-x-4">
+                <a href="#" class="text-white">Inicio</a>
+                <a href="#" class="text-white">Acerca de</a>
+                <a href="#" class="text-white">Servicios</a>
+                <a href="#" class="text-white">Contacto</a>
+            </div>
+        </div>
+    </nav>
