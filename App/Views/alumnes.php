03cac86f13aa94358980445e47ae5241cdc461f5
Roman Mysyura <rmysyura@cendrassos.net>
2023-11-28 17:36:52 +0100

    design updated

--- App/Views/alumnes.php
+++ App/Views/alumnes.php
@@ -30,7 +30,6 @@
                 </div>
             </div>
 
-
         </div>
     </div>
 
@@ -71,23 +70,82 @@
                     <td class="text-lg"><?= $alumne['user_birth_date'] ?></td>
                     <td class="text-lg"><?= $alumne['user_phone'] ?></td>
                     <td class="text-lg"><?= $alumne['photo_url'] ?></td>
-                    <td class="text-lg"><button type="submit" class="btn btn-active btn-neutral mr-auto">Seleccionar foto</button></td>
-                    <th>
-                        <form action="/uploadPhotoFromFile" method="post" enctype="multipart/form-data"
-                            class="flex items-center ">
-                            <input type="hidden" name="user_id" value="<?= $alumne['user_id'] ?>">
-                            <input type="file" name="photo" accept="image/jpeg, image/jpg, image/png"
-                                class="ml-auto mr-auto">
-                            <button type="submit" class="btn btn-active btn-neutral mr-auto">Pujar Foto</button>
-                        </form>
-                    </th>
+                    <td class="text-lg">
+                        <button class="btn" onclick="openModal('<?= $alumne['user_id'] ?>')">Seleccionar Foto</button>
+                    </td>
+
+
+
                 </tr>
+                <dialog id="my_modal_2" class="modal">
+                    <div class="modal-box">
+                        <h3 id="modalTitle" class="font-bold text-lg">Hello!</h3>
+
+
+
+
+                        <form action="/uploadPhotoFromFile" method="post" enctype="multipart/form-data"
+                            class="flex items-center">
+                            <input type="hidden" name="user_id" id="userIdInput" value="">
+
+                            <div class="flex items-center justify-center w-full">
+                                <label for="dropzone-file"
+                                    class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                    <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                                        <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="2"
+                                                d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                                        </svg>
+                                        <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span
+                                                class="font-semibold">Clica</span> o arrastra la foto</p>
+                                        <p class="text-xs text-gray-500 dark:text-gray-400">JPG, JPEG i PNG</p>
+                                    </div>
+                                    <input type="file" id="dropzone-file" name="photo"
+                                        accept="image/jpeg, image/jpg, image/png" class="">
+
+
+                                </label>
+                            </div>
+                            <button type="submit" class="btn btn-active btn-neutral mr-auto mt-2">Pujar Foto</button>
+                        </form>
+
+                        <!-- Botón para cerrar el modal -->
+                        <button class="btn btn-active btn-neutral ml-auto mt-2" onclick="closeModal()" >Cancelar</button>
+                    </div>
+                </dialog>
+
                 <?php endforeach; ?>
         </table>
+        <!-- Open the modal using ID.showModal() method -->
+
+
     </div>
 
     <?php include "footer.php" ?>
     <script>
+    function openModal(userId) {
+        // Obtener el elemento del título del modal
+        var modalTitle = document.getElementById('modalTitle');
+
+        // Actualizar el contenido del título con el valor de userId
+        modalTitle.innerText = 'User ID: ' + userId;
+
+        // Obtener el elemento de entrada oculta para el user_id en el formulario
+        var userIdInput = document.getElementById('userIdInput');
+
+        // Actualizar el valor del campo user_id en el formulario
+        userIdInput.value = userId;
+
+        // Mostrar el modal
+        document.getElementById('my_modal_2').showModal();
+    }
+
+    function closeModal() {
+        // Cerrar el modal
+        document.getElementById('my_modal_2').close();
+    }
     // Add JavaScript to handle search functionality
     document.getElementById('searchInput').addEventListener('input', function() {
         var searchQuery = this.value.toLowerCase();
