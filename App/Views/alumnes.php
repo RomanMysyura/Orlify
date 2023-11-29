bfb6d04a720712692d70f897909934904d563e75
Roman Mysyura <rmysyura@cendrassos.net>
2023-11-29 16:48:15 +0100

    bug fix

--- App/Views/alumnes.php
+++ App/Views/alumnes.php
@@ -16,10 +16,10 @@
     <div class="ml-0 w-full">
         <div class="editar_usuari">
 
-            <div class='max-w-md mx-auto mb-5'>
+            <div class='max-w-md mx-auto mb-5 '>
                 <div
                     class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
-                    <div class="grid place-items-center h-full w-12 text-gray-300">
+                    <div class="grid place-items-center h-full w-12 text-gray-300 bg-slate-200">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                             stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -27,8 +27,8 @@
                         </svg>
                     </div>
 
-                    <input class="peer h-full w-full outline-none text-sm text-gray-700 pr-2" title="search" type="text"
-                        id="searchInput" placeholder="Buscar por nombre, correo, etc." />
+                    <input class="peer h-full w-full outline-none text-lg text-gray-700 pr-2 bg-slate-200" title="search" type="text"
+                        id="searchInput" placeholder="Buscar alumne per el seu nom, correu... etc." />
                 </div>
             </div>
 
@@ -73,7 +73,7 @@
                     <td class="text-lg"><?= $alumne['user_phone'] ?></td>
                     <td class="text-lg"><?= $alumne['photo_url'] ?></td>
                     <td class="text-lg">
-                        <button class="btn modalFoto" data-user-id="<?= $alumne['user_id'] ?>">Seleccionar Foto</button>
+                        <button class="btn modalFoto" data-user-id="<?= $alumne['user_id'] ?>">Actualitzar Foto</button>
                     </td>
 
                 </tr>
