97e62ca9e76460ac9e301decd07c2d6ff4be3404
ecardosa <ecardosa@cendrassos.net>
2023-12-01 19:34:02 +0100

    a

--- App/Views/paneldecontrol.php
+++ App/Views/paneldecontrol.php
@@ -49,7 +49,7 @@
                                         d="M14.9536 14.9458L21 21M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
                                         stroke="#999" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                                 </g>
-
+<!-- a -->
                             </svg>
                         </button>
                     </div>
