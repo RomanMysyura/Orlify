7ce912be2ff56bc11ad94079ae50dbf052468347
Roman Mysyura <rmysyura@cendrassos.net>
2023-12-11 17:19:54 +0100

    New merge

--- App/Views/alumnes.php
+++ App/Views/alumnes.php
@@ -144,7 +144,7 @@
             
                         </form>
                         <form method="dialog" style="display: inline-block">
-                        <button type="button" class="btn btn-active btn-neutral ml-auto mt-2rounded  before:ease relative h-12 w-40 overflow-hidden border border-grey-800 bg-grey-800 text-grey-300 shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:shadow-gray-800 hover:before:-translate-x-40"  id="modalCancelar">Cancelar</button>
+                        <button class="btn btn-active btn-neutral ml-auto mt-2rounded  before:ease relative h-12 w-40 overflow-hidden border border-grey-800 bg-grey-800 text-grey-300 shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-12 before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:shadow-gray-800 hover:before:-translate-x-40"  id="modalCancelar">Cancelar</button>
                     </form>
 
                     </div>
