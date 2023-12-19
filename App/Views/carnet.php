c162c2b4c409df9015a53cbe99c2d8bb14dd3762
Marcos Muñoz <mmunoz2@cendrassos.net>
2023-12-19 17:11:03 +0100

    servidor

--- App/Views/carnet.php
+++ App/Views/carnet.php
@@ -33,7 +33,7 @@
                     <p class="text-gray-500 text-2xl"><?= $user['role']; ?></p>
                     <a
                         class="text-xl text-gray-600 dark:text-gray-200 group-hover:opacity-100 opacity-0 transform transition-all delay-300 duration-500">
-                        <?= $user['email']; ?>
+                        <?= $user['email'];?>
                     </a>
                     <br>
                     <a
